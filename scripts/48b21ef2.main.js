1f5941633e42b094889050761a004d2b283bc5c8
Peter Tseng <patseng@stanford.edu>
2013-09-06 14:47:50 -0700

    Updated deploy

---
+++ scripts/48b21ef2.main.js
@@ -0,0 +1,1 @@
+(function(){window.Projects={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";return console.log("Hello from Backbone!")}},$(function(){"use strict";return Projects.init()})}).call(this),this.JST=this.JST||{};
